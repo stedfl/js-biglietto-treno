6c2c777b867380511fe958410644a71b1fd4ae73
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-11 16:06:38 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const userKm = 35;
-const userAge = 20;
+const userKm = parseInt(prompt("Quanti km vuoi percorrere?"));
+const userAge = parseInt(prompt("Quanti anni hai?"))
 const priceForKm = 0.21;
 const totalPrice = userKm * priceForKm;
 console.log(totalPrice);
@@ -7,21 +7,22 @@ let discount;
 let valorDiscount;
 let finalPrice;
 
-
 if (userAge >= 65) {
   discount = 0.4;
-  valorDiscount = totalPrice * discount;
-  finalPrice = totalPrice - valorDiscount;
-  console.log(finalPrice);
-} else if (userAge <= 17) {
+}  else if (userAge <= 17) {
   discount = 0.2;
-  valorDiscount = totalPrice * discount;
-  finalPrice = totalPrice - valorDiscount;
-  console.log(finalPrice);
 } else {
-  discount = 0;
-  valorDiscount = totalPrice * discount;
-  finalPrice = totalPrice - valorDiscount;
-  console.log(finalPrice);
+  discount = 0; 
 }
+
+valorDiscount = totalPrice * discount;
+finalPrice = totalPrice - valorDiscount;
+finalPriceRounded = finalPrice.toFixed(2);
+const output = `
+In base alla tua età e ai chilometri che vuoi percorrere, il prezzo del biglietto è di ${finalPriceRounded}€. 
+` ;
+
+document.getElementById("price").innerHTML = output;
+
+
 
