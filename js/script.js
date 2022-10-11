4e417381e6fdb2e5bfe7c566d3d5630c5c557187
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-11 16:29:40 +0200

    add math.round  for rounding price to decimal

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const userKm = parseInt(prompt("Quanti km vuoi percorrere?"));
 const userAge = parseInt(prompt("Quanti anni hai?"))
 const priceForKm = 0.21;
 const totalPrice = userKm * priceForKm;
-console.log(totalPrice);
 let discount;
 let valorDiscount;
 let finalPrice;
@@ -17,7 +16,7 @@ if (userAge >= 65) {
 
 valorDiscount = totalPrice * discount;
 finalPrice = totalPrice - valorDiscount;
-finalPriceRounded = finalPrice.toFixed(2);
+finalPriceRounded = Math.round(finalPrice * 100) / 100;
 const output = `
 In base alla tua età e ai chilometri che vuoi percorrere, il prezzo del biglietto è di ${finalPriceRounded}€. 
 ` ;
