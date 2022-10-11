c92ff377740d354d662debe7a3a71a9ea93b455b
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-11 17:28:27 +0200

    change method
    from math toFixed method for rouding decimal

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
-const userKm = parseInt(prompt("Quanti km vuoi percorrere?"));
-const userAge = parseInt(prompt("Quanti anni hai?"))
+const userKm = 10;
+const userAge = 30;
+// const userKm = parseInt(prompt("Quanti km vuoi percorrere?"));
+// const userAge = parseInt(prompt("Quanti anni hai?"))
 const priceForKm = 0.21;
 const totalPrice = userKm * priceForKm;
 let discount;
 let valorDiscount;
 let finalPrice;
+let visualDiscount;
 
 if (userAge >= 65) {
   discount = 0.4;
+  visualDiscount = `Essendo tu non più giovanissimo, hai diritto ad uno sconto del 40%`;
 }  else if (userAge <= 17) {
   discount = 0.2;
+  visualDiscount = `Essendo tu molto giovane, hai diritto ad uno sconto del 20%.`;
 } else {
   discount = 0; 
+  visualDiscount = "";
 }
 
 valorDiscount = totalPrice * discount;
 finalPrice = totalPrice - valorDiscount;
-finalPriceRounded = Math.round(finalPrice * 100) / 100;
+finalPriceRounded = finalPrice.toFixed(2);
+
+const age = `
+Ciao, oggi hai scelto di percorrere ${userKm}Km per raggiungere il posto dei tuoi sogni.
+`;
+
 const output = `
 In base alla tua età e ai chilometri che vuoi percorrere, il prezzo del biglietto è di ${finalPriceRounded}€. 
 ` ;
 
+document.getElementById("hello").innerHTML = age;
+document.getElementById("discounting").innerHTML = visualDiscount;
 document.getElementById("price").innerHTML = output;
 
 
